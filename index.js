b6a1bf37adbaf8632b94992bfa7b8ba8b2ffe7f9
Benjamin E. Coe <bencoe@gmail.com>
2016-05-14 14:39:47 -0700

    chore: add Travis configuration (#1)
    * chore: add Travis configuration
    
    * chore: standard nits

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function (blocking) {
-  [process.stdout, process.stderr].forEach(function(stream){
+  [process.stdout, process.stderr].forEach(function (stream) {
     if (stream._handle && typeof stream._handle.setBlocking === 'function') {
       stream._handle.setBlocking(blocking)
     }
