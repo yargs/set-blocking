1de964d9e5f329eca277fa742e0a1fc3fd7d1dde
Max Rittmüller <max.rittmueller@gmail.com>
2016-12-18 01:03:39 +0100

    fix: windows stuff

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ require('chai').should()
 describe('setBlocking', function () {
   // see: https://github.com/yargs/yargs/issues/497
   it('does not truncate text printed to stdout when process.exit() is called', function (done) {
-    exec('./test/fixtures/yargs-497-stdout.js', function (err, stdout, stderr) {
+    exec('node ./test/fixtures/yargs-497-stdout.js', function (err, stdout, stderr) {
       if (err) return done(err)
       stdout.should.match(/line 2999/)
       return done()
@@ -15,7 +15,7 @@ describe('setBlocking', function () {
   })
 
   it('does not truncate text printed to stderr when process.exit() is called', function (done) {
-    exec('./test/fixtures/yargs-497-stderr.js', function (err, stdout, stderr) {
+    exec('node ./test/fixtures/yargs-497-stderr.js', function (err, stdout, stderr) {
       err.should.match(/Command failed/)
       stderr.should.match(/line 2999/)
       return done()
